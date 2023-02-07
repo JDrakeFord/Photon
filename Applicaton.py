65d8816a0aa438ca57917baa7d0be8680a68d8ef
Xaffels <jpharris68@gmail.com>
2023-02-07 13:54:20 -0600

    Splash Screen Starter

--- Applicaton.py
+++ Applicaton.py
@@ -1,1 +1,24 @@
-print("hello world")
+from tkinter import *
+from PIL import ImageTk, Image
+
+#TODO: Fix the image and make it resize with the window
+
+#Creates the screen and sets the size
+screen = Tk()
+screen.geometry("1280x720")
+
+#Puts the Image onto the screen
+imgframe = Frame(screen, width=1280, height=720)
+imgframe.pack()
+imgframe.place(anchor = 'center')
+img = ImageTk.PhotoImage(Image.open("SplashLogo.jpg"))
+splash = Label(imgframe, image = img)
+splash.pack()
+
+#Function to delete the splashscreen
+def deleteSplash():
+    splash.destroy()    
+
+screen.after(1250, deleteSplash)
+
+mainloop()
