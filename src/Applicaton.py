ca18033a11cfd840c6c1f1af0133d1a70f0aaf92
banks <banks47j@gmail.com>
2023-02-18 15:56:03 -0600

    Fixed integration between Applicaion.py and Entry.py
    I commented out the integration of the splash screen on Application.py due to it causing crashes. We'll need to fix that.
    
    Expanded Entry screen to have entries for first name, last name, and code name. We should pull the id and key from the counter variable (i) when we are ready to use that data.

--- src/Applicaton.py
+++ src/Applicaton.py
@@ -1,6 +1,23 @@
-import Splash
+#Commented out due to application not running with it. Out of date implementation?
+
+#import Splash
+#Splash.drive()
+
+# Drive player entry screen
+from tkinter import Tk
 from Entry import Entry
+from src.Entry.Entry import PlayerEntryScreen
 
-Splash.drive()
-Entry.drive()
 
+def main():
+    # create the Tk object
+    root = Tk()
+
+    # create an instance of PlayerEntryScreen, passing the Tk object as the master
+    player_entry_screen = PlayerEntryScreen(root)
+
+    # call the drive() method of the instance to start the tkinter application
+    player_entry_screen.drive()
+
+if __name__ == '__main__':
+    main()
--- src/Entry/Entry.py
+++ src/Entry/Entry.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 class PlayerEntryScreen:
     def __init__(self, master):
         self.master = master
@@ -10,33 +9,61 @@ class PlayerEntryScreen:
         self.team_label1 = tk.Label(master, text="Red Team")
         self.team_label1.grid(row=0, column=1, padx=10, pady=10)
         self.team_label2 = tk.Label(master, text="Green Team")
-        self.team_label2.grid(row=0, column=3, padx=10, pady=10)
+        self.team_label2.grid(row=0, column=6, padx=10, pady=10)
 
         self.name_labels = []
-        self.name_entries = []
+        self.first_name_entries = []
+        self.last_name_entries = []
+        self.code_name_entries = []
 
         for i in range(20):
             i_str = str(i+1)
-            name_label1 = tk.Label(master, text="Player #" + i_str + " Name:")
+            name_label1 = tk.Label(master, text="Player #" + i_str + " First Name:")
             name_label1.grid(row=i+1, column=0, padx=5, pady=5)
-            name_entry1 = tk.Entry(master)
-            name_entry1.grid(row=i+1, column=1, padx=5, pady=5)
+            first_name_entry1 = tk.Entry(master)
+            first_name_entry1.grid(row=i+1, column=1, padx=5, pady=5)
+            last_name_label1 = tk.Label(master, text="Last Name:")
+            last_name_label1.grid(row=i+1, column=2, padx=5, pady=5)
+            last_name_entry1 = tk.Entry(master)
+            last_name_entry1.grid(row=i+1, column=3, padx=5, pady=5)
+            code_name_label1 = tk.Label(master, text="Code Name:")
+            code_name_label1.grid(row=i+1, column=4, padx=5, pady=5)
+            code_name_entry1 = tk.Entry(master)
+            code_name_entry1.grid(row=i+1, column=5, padx=5, pady=5)
+
             self.name_labels.append(name_label1)
-            self.name_entries.append(name_entry1)
+            self.first_name_entries.append(first_name_entry1)
+            self.last_name_entries.append(last_name_entry1)
+            self.code_name_entries.append(code_name_entry1)
 
-            name_label2 = tk.Label(master, text="Player #" + i_str + " Name:")
-            name_label2.grid(row=i+1, column=3, padx=5, pady=5)
-            name_entry2 = tk.Entry(master)
-            name_entry2.grid(row=i+1, column=4, padx=5, pady=5)
+        for i in range(20):
+            i_str = str(i+1)
+            name_label2 = tk.Label(master, text="Player #" + i_str + " First Name:")
+            name_label2.grid(row=i+1, column=6, padx=5, pady=5)
+            first_name_entry2 = tk.Entry(master)
+            first_name_entry2.grid(row=i+1, column=7, padx=5, pady=5)
+            last_name_label2 = tk.Label(master, text="Last Name:")
+            last_name_label2.grid(row=i+1, column=8, padx=5, pady=5)
+            last_name_entry2 = tk.Entry(master)
+            last_name_entry2.grid(row=i+1, column=9, padx=5, pady=5)
+            code_name_label2 = tk.Label(master, text=" Code Name:")
+            code_name_label2.grid(row=i+1, column=10, padx=5, pady=5)
+            code_name_entry2 = tk.Entry(master)
+            code_name_entry2.grid(row=i+1, column=11, padx=5, pady=5)
+
             self.name_labels.append(name_label2)
-            self.name_entries.append(name_entry2)
+            self.first_name_entries.append(first_name_entry2)
+            self.last_name_entries.append(last_name_entry2)
+            self.code_name_entries.append(code_name_entry2)
 
+    def submit(self):
+        # Add code here to handle the submission of player names
+        for name_entry in self.name_entries:
+            print(name_entry.get())
+
+    def drive(self):
         # Create submit button
-        self.submit_button = tk.Button(master, text="Submit", command=self.submit)
-        self.submit_button.grid(row=21, column=3, padx=10, pady=10)
+        submit_button = tk.Button(self.master, text="Submit", command=self.submit)
+        submit_button.grid(row=21, column=3, padx=10, pady=10)
 
-
-# Create main window and run application
-root = tk.Tk()
-app = PlayerEntryScreen(root)
-root.mainloop()
+        self.master.mainloop()
