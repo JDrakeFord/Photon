aa6ebbda3f607cdd8d174bf3ec142bead11d7cdb
JISAC9M <jesse.d.ford@jbhunt.com>
2023-02-22 11:09:03 -0600

    Refactored and added classes for new Data structures

---
+++ src/Data/Player.py
@@ -0,0 +1,1 @@
+# Player class - represents players in the game. Will contain player score, name (first and last), and codename
---
+++ src/Data/Team.py
@@ -0,0 +1,2 @@
+# Team class - represents a team in the game. Will contain a list of players on the team, team score,
+# and other necessary information
