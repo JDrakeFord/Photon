1abc47703cb6d1cba783025ca41b92a1093dc701
TatteredKeycaps <ozark.joseph@protonmail.com>
2023-04-16 12:28:59 -0500

    Fixed 6 min timer formatting, noticed timeout errors when entering player names

--- src/Display/Action.py
+++ src/Display/Action.py
@@ -122,7 +122,7 @@ class PlayerActionScreen(tk.Frame):
         
     def update_time_left_game(self):
         min, sec = divmod(self.time_left, 60)
-        gameTimeLeft = "%02d:%02d" % (min, sec)
+        gameTimeLeft = "%2d:%02d" % (min, sec)
         self.time_left_label.config(text=f'Time remaining before game end: {gameTimeLeft}')
         self.time_left -= 1
         if self.time_left >= 0:
