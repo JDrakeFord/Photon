c02a5413d9565201608b576df0b062ae7d6eb91d
banks <banks47j@gmail.com>
2023-04-16 12:49:53 -0500

    Minor formatting and fixes

--- src/Display/Splash.py
+++ src/Display/Splash.py
@@ -1,24 +1,20 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-
 def drive():
-    # Creates the screen and sets the size
     screen = Tk()
     screen.geometry("720x480")
+    # Creates the screen and set size
 
-    # Puts the Image onto the screen
+    # Puts splash image on screen then delete
     imgframe = Frame(screen, width=720, height=480)
     imgframe.pack()
     imgframe.place(anchor='nw')
     img = ImageTk.PhotoImage(Image.open("./Resources/Splash480p.jpg"))
     splash = Label(imgframe, image=img)
     splash.pack()
-
-    # Function to delete the splashscreen
     def deleteSplash():
         screen.destroy()
-
     screen.after(1250, deleteSplash)
 
     mainloop()
